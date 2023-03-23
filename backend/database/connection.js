4b3279b0b344087fdeae6baaaa4ecf0383fd9809
FREDVUNI <fredvuni809@gmail.com>
2023-03-23 15:53:38 +0300

    Fix connection

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async() =>{
     try{
         mongoose.set('strictQuery',false)
-        const con = await mongoose.connect(process.env.MONGO_URI,{
-            useUnifiedTopology:true,
-            useNewUrlParser:true
+        mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
         })
         console.log(`connected to database.`)
     }
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv"
 import connectDB from "./database/connection.js";
-import router from './routes/Bootcamp.js'
+// import BootcampRouter from './routes/Bootcamp.js';
 
 const app = express();
 dotenv.config({ path:".env" })
@@ -12,9 +12,9 @@ app.use(morgan("tiny"))
 app.use(express.urlencoded({extended:false}));
 app.use(express.json())
 
-app.use("/api/v1/",router)
+// app.use("/api/v1/",BootcampRouter)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 connectDB()
 
 app.listen(PORT,() =>{
