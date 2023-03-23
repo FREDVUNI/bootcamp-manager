5b9e41586b13f4aba3c0b3c6935073244c8f6d30
FREDVUNI <fredvuni809@gmail.com>
2023-03-23 14:44:13 +0300

    Finish database connection

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async() =>{
-    
+    try{
+        mongoose.set('strictQuery',false)
+        const con = await mongoose.connect(process.env.MONGO_URI,{
+            useUnifiedTopology:true,
+            useNewUrlParser:true
+        })
+        console.log(`connected to database.`)
+    }
+    catch(error){
+        console.log(error.message)
+        process.exit(1)
+    }
 }
 
 export default connectDB
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv"
+import connectDB from "./database/connection.js";
+
+const app = express();
+dotenv.config({ path:".env" })
+
+app.use(morgan("tiny"))
+
+const PORT = process.env.PORT;
+connectDB()
+
+app.listen(PORT,() =>{
+    console.log(`server is listening on http://localhost:${PORT}`)
+})
