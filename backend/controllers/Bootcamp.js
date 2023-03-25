f5b64f793a49e6df105f16a49153da9d196f38be
FREDVUNI <fredvuni809@gmail.com>
2023-03-25 15:48:53 +0300

    bootcamp by id exception

--- backend/controllers/Bootcamp.js
+++ backend/controllers/Bootcamp.js
@@ -1,128 +1,120 @@
-import Bootcamp from "../models/Bootcamp.js"
+import Bootcamp from "../models/Bootcamp.js";
 
-export const getBootcamps = async(req,res) =>{
-    try{
-        const bootcamps = await Bootcamp.find()
-        return res.status(200).json({
-            success:true,
-            data:bootcamps
-        })
+export const getBootcamps = async (req, res) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    return res.status(200).json({
+      success: true,
+      data: bootcamps,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+export const createBootcamp = async (req, res) => {
+  try {
+    const { name, rating, description, price } = req.body;
+
+    const exists = await Bootcamp.findOne({
+      name,
+    });
+
+    if (exists)
+      return res.status(400).json({
+        error: `The ${name.toLowerCase()} bootcamp already exists.`,
+      });
+
+    const bootcamp = new Bootcamp({
+      name,
+      rating,
+      description,
+      price,
+    });
+
+    let new_bootcamp = await bootcamp.save();
+    return res.status(201).json({
+      success: true,
+      message: `${name} bootcamp has been saved.`,
+      data: new_bootcamp,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+export const getBootcamp = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const bootcamp = await Bootcamp.findById(id);
+    if (!bootcamp) {
+      return res.status(404).json({
+        error: "Bootcamp was not found.",
+      });
     }
-    catch(error){
-        return res.status(500).json({
-            error:error.message
-        })
-    }
-}
 
-export const createBootcamp = async(req,res) =>{
-    try{
-        const { name,rating,description,price } = req.body
+    return res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
-        const exists = await Bootcamp.findOne({
-            name
-        })
+export const updateBootcamp = async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    const bootcamp = await Bootcamp.findById(id);
 
-        if(exists) return res.status(400).json({
-            error:`The ${name.toLowerCase()} bootcamp already exists.`,
-        })
+    if (!bootcamp)
+      return res.status(404).json({
+        error: "Bootcamp was not found.",
+      });
 
-        const bootcamp = new Bootcamp({
-            name,
-            rating,
-            description,
-            price
-        })
+    bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-        let new_bootcamp = await bootcamp.save()
-        return res.status(201).json({
-            success:true,
-            message:`${name} bootcamp has been saved.`,
-            data:new_bootcamp
-        })
-    }
-    catch(error){
-        return res.status(500).json({
-            error:error.message
-        })
-    }
-}
+    return res.status(201).json({
+      success: true,
+      message: `${req.body.name} bootcamp has been updated.`,
+      data: bootcamp,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
-export const getBootcamp = async(req,res) =>{
-    try{
-        const id = req.params.id
-        const bootcamp = await Bootcamp.findById(id)
-        
-        console.log(bootcamp._id)
-        const bootcampId = await Bootcamp.findOne({
-            _id:id
-        })
+export const deleteBootcamp = async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    const bootcamp = await Bootcamp.findById(id);
 
-        if(!bootcampId) return res.status(404).json({
-            error:"Bootcamp was not found.",
-        })
+    if (!bootcamp)
+      return res.status(404).json({
+        error: "Bootcamp was not found.",
+      });
 
-        return res.status(200).json({
-            success:true,
-            data:bootcamp
-        })
+    await Bootcamp.remove();
 
-    }
-    catch(error){
-        return res.status(500).json({
-            error:error.message
-        })
-    }
-}
-
-export const updateBootcamp = async(req,res) =>{
-    try{
-        const { id } = req.params.id
-        const bootcamp = await Bootcamp.findById(id)
-        
-        if(!bootcamp) return res.status(404).json({
-            error:"Bootcamp was not found.",
-        })
-
-        bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id,req.body,{
-            new:true,
-            runValidators:true
-        })
-
-        return res.status(201).json({
-            success:true,
-            message:`${req.body.name} bootcamp has been updated.`,
-            data:bootcamp
-        })
-
-    }
-    catch(error){
-        return res.status(500).json({
-            error:error.message
-        })
-    }
-}
-
-export const deleteBootcamp = async(req,res) =>{
-    try{
-        const { id } = req.params.id
-        const bootcamp = await Bootcamp.findById(id)
-        
-        if(!bootcamp) return res.status(404).json({
-            error:"Bootcamp was not found.",
-        })
-
-        await Bootcamp.remove()
-
-        return res.status(201).json({
-            success:true,
-            message:`${req.body.name} bootcamp has been deleted.`,
-            data:{}
-        })
-    }
-    catch(error){
-        return res.status(500).json({
-            error:error.message
-        })
-    }
-}
+    return res.status(201).json({
+      success: true,
+      message: `${req.body.name} bootcamp has been deleted.`,
+      data: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
