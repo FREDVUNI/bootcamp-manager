53bca65957e4b8bc960b6c3c9c895646621637a4
FREDVUNI <fredvuni809@gmail.com>
2023-04-02 12:27:15 +0300

    create the notfound page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
       </Router>
       <Footer/>
