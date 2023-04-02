106e1a2cae3fd0a23b9a700153a0abe7e8224e42
FREDVUNI <fredvuni809@gmail.com>
2023-04-02 12:59:34 +0300

    adjust using prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import Footer from './components/Footer'
-import Hero from './components/Hero'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-import { BootcampProvider } from "./context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import BootcampProvider from "./context";
 
 function App() {
   return (
     <div>
-      <Hero/>
+      <Hero />
       <BootcampProvider>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='*' element={<NotFound/>}/>
-        </Routes>
-      </Router>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
       </BootcampProvider>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
-import { Box,Container } from "@mui/system";
+import { Box, Container } from "@mui/system";
 import { CircularProgress, Grid } from "@mui/material";
 import BootcampCard from "../components/BootcampCard";
+import { bootcampContext } from "../context";
 
 const Home = () => {
+  const { bootcamps, setBootcamps, loading } = useContext(bootcampContext);
 
-  const { bootcamps,setBootcamps } = useContext()
-
-  console.log(bootcamps);
+  console.log(process.env.BASE_URL);
   return (
     <Box
       sx={{
@@ -17,38 +17,38 @@ const Home = () => {
         gap: "2.5rem",
       }}
     >
-      <Container sx={{ marginTop:'20px' }}>
+      <Container sx={{ marginTop: "20px" }}>
         <Grid container spacing={2}>
-          { 
-            loading ? (
-            <div sx={{
-                width:'100%',
-                display:'flex',
-                justifyContent:'center',
-                alignItems:'center'
-            }}>
-              <CircularProgress size="3rem"  thickness={5}/>
+          {loading ? (
+            <div
+              sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CircularProgress size="3rem" thickness={5} />
             </div>
-            ):(
-              bootcamps.length > 0 ? (
-              bootcamps.map((bootcamp) =>(
-               <Grid item key={bootcamp._id} xs={12} sm={6} md={4} lg={3}>
-                <BootcampCard bootcamp={bootcamp}/>
-               </Grid>
-              ))
-              ):(
-                <p sx={{
-                  width:'100%',
-                  display:'flex',
-                  justifyContent:'center',
-                  alignItems:'center',
-                  fontSize:'18px'
-                }}>
-                  There are bootcamps
-                </p>
-              )
-            )
-          }
+          ) : bootcamps.length > 0 ? (
+            bootcamps.map((bootcamp) => (
+              <Grid item key={bootcamp._id} xs={12} sm={6} md={4} lg={3}>
+                <BootcampCard bootcamp={bootcamp} />
+              </Grid>
+            ))
+          ) : (
+            <p
+              sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "18px",
+              }}
+            >
+              There are bootcamps
+            </p>
+          )}
         </Grid>
       </Container>
     </Box>
